e2a5c131e6494c9aa71f728332e08f2309e7fe07
amanda-zakir <amandazakir@gmail.com>
2026-02-12 16:32:01 +0100

    test metrics 1

--- src/test/java/dummyTest.java
+++ src/test/java/dummyTest.java
@@ -36,6 +36,6 @@ public class dummyTest {
     public void negClassifyTriangle() {
         dummy d = new dummy();
 
-        d.classifyTriangle(-2.0, 2.0, 2.0);
+        d.classifyTriangle(-2.0, 2.0, 2.0)
     }
 }
